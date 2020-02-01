3add22427af49a87a3e5557e04607d6651e18ebb
yaminiPonugoti <yponugot@purdue.edu>
2020-02-01 17:42:21 -0500

    created basic files for tumblr

---
+++ PhotoCell.swift
@@ -0,0 +1,25 @@
+//
+//  PhotoCell.swift
+//  tumblr
+//
+//  Created by Yamini Ponugoti on 1/29/20.
+//  Copyright © 2020 Yamini Ponugoti. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UITableViewCell {
+
+    @IBOutlet weak var imgView: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
